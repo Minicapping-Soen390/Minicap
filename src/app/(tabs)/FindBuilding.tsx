bba3bb0d82b049ade8bd20182fe9f34eba7bff1e
Edward M. Villamaria <edwardvillamaria@gmail.com>
2025-02-18 11:37:45 -0500

    refactoring FindBuilding.tsx to utilize globalStyles.ts instead of inline

--- src/app/(tabs)/FindBuilding.tsx
+++ src/app/(tabs)/FindBuilding.tsx
@@ -1,13 +1,14 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { globalStyles, mainEdges } from "../styles/globalStyles";
 
 const FindBuilding = () => {
   return (
-    <SafeAreaView
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    >
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Find Building</Text>
+    <SafeAreaView style={globalStyles.container} edges={mainEdges}>
+      <Text style={[globalStyles.textBold, { fontSize: 24 }]}>
+        Find Building
+      </Text>
     </SafeAreaView>
   );
 };
