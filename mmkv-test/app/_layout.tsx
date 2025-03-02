ed7cd419f1e6fd9ed144b8da7441d34886f87aee
vibqetowi <abutcher.usa@protonmail.com>
2025-03-02 13:47:08 -0500

    checkout

--- mmkv-test/app/_layout.tsx
+++ mmkv-test/app/_layout.tsx
@@ -5,7 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { MMKVLoader, useMMKVStorage } from "react-native-mmkv-storage";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -27,7 +27,12 @@ export default function RootLayout() {
     return null;
   }
 
+  const buildingStorage = new MMKVLoader()
+  .withInstanceID("buildingStorage")
+  .initialize();
+
   return (
+
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
